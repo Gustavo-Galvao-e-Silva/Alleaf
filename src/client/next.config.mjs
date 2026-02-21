7a97d8b05c10ed99fefbe3d531d9f019f13cbd59
kalyanoliveira <kalyan.coliveira@gmail.com>
2026-02-20 22:36:45 -0800

    tells nextjs to not bundle large ai deps

--- src/client/next.config.mjs
+++ src/client/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: { serverComponentsExternalPackages: ['@xenova/transformers'] },
 };
-
 export default nextConfig;
