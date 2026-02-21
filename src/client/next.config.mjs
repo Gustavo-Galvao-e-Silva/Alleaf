e1f4ea7730c4bbdff2f09a4807b50672df83c0bb
kalyanoliveira <kalyan.coliveira@gmail.com>
2026-02-21 12:14:44 -0800

    chore: updates next.config.mjs

--- src/client/next.config.mjs
+++ src/client/next.config.mjs
@@ -1,5 +1,7 @@
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: { serverComponentsExternalPackages: ['@xenova/transformers'] },
+  serverExternalPackages: ['@xenova/transformers'],
 };
+
 export default nextConfig;
